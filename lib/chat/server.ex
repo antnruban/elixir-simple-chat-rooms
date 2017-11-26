796560da5a8c41e3844895e29c8781c1aa259b5f
antnruban <anton.ruban@chsiw.com>
2017-11-26 21:53:41 +0200

    initial commit

---
+++ lib/chat/server.ex
@@ -0,0 +1,34 @@
+defmodule Chat.Server do
+  use GenServer
+
+  # API
+  def start_link(name) do
+    GenServer.start_link(__MODULE__, [], name: via_tuple(name))
+  end
+
+  def add_message(room_name, message) do
+    GenServer.cast(via_tuple(room_name), {:add_message, message})
+  end
+
+  def get_messages(room_name) do
+    GenServer.call(via_tuple(room_name), :get_messages)
+  end
+
+  defp via_tuple(room_name) do
+    {:via, Chat.Registry, {:chat_room, room_name}}
+  end
+
+  # SERVER
+  def init(messages) do
+    {:ok, messages}
+  end
+
+  def handle_cast({:add_message, new_message}, message) do
+    IO.puts [new_message | message]
+    {:noreply, [new_message | message]}
+  end
+
+  def handle_call(:get_messages, _from, messages) do
+    {:reply, messages, messages}
+  end
+end
